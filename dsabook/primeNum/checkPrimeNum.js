c4f7b73631401ac76240965a00c619028a642ed0
pnquynhdev <pnquynh.dev@gmail.com>
2024-09-26 14:45:21 +0700

    function to chek isPrime

--- dsabook/primeNum/checkPrimeNum.js
+++ dsabook/primeNum/checkPrimeNum.js
@@ -1,12 +1,15 @@
-function isPrime(num) {
-    if (num <= 1) return false; // Numbers less than or equal to 1 are not prime
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false; // If num is divisible by any number other than 1 and itself
-        }
-    }
-    return true; // If no divisors were found, num is a prime number
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+  for (let i = 2; i < Math.sqrt(n); i++) {
+    if (n % i == 0) return false;
+  }
+  return true;
 }
-// Example of user input (in a real-world scenario, this could be from a form or console input)
-const userInput = 11;
-console.log(`Is ${userInput} a prime number? `, isPrime(userInput));
+
+console.log(isPrime(3));
+console.log(isPrime(100));
+console.log(isPrime(300));
+console.log(isPrime(11));
+
